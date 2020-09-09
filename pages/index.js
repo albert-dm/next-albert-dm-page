5310fa680d5e8f39cb0163407abb8ad5bac6762c
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-09-08 21:46:04 -0300

    configuring files next.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+ h1 {
+   font-size: 4rem;
+ }
+`;
+const Container = styled.div`
+  text-align: center;
+`;
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SSR styled-components with Next.js Starter</title>
+      </Head>
+      <Container>
+        <GlobalStyle />
+        <h1>Hello, world!</h1>
+      </Container>
+    </>
+  );
+}
