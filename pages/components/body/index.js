128f6f09a25037264d8795fcea547c4769904226
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-09-14 10:19:40 -0300

    eslint configuration

--- pages/components/body/index.js
+++ pages/components/body/index.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <BodyStyled>
       <img src="/profile.jpg" alt="my image" className="profilePhoto" />
-      <div className="profileText">
+        <div className="profileText">
         <h1 className="title">{'>'}DEV</h1>
         <p className="description">Software Developer and Automation Engineer Building applications that suits your business Web, Mobile, Desktop and even IoT</p>
         <p className="descriptionTwo">Any buisiness can and needs to be digital and in the web. If you want to talk about it, fell free to contact me!</p>
