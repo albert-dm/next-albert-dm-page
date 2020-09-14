3e7c998112f433c40c05a3720d70556737636300
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-09-14 19:56:00 -0300

    eslint configuration version two

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 
-import React from 'react'
-import clients from './api/clientsList'
-import CardContainer from './components/cardContainer'
-import Body from './components/body'
-import Header from './components/header'
+import React from 'react';
+import clients from './api/clientsList';
+import CardContainer from './components/cardContainer';
+import Body from './components/body';
+import Header from './components/header';
 
 const App = () => {
-    return (
-        <div>
-            <Header/>
-            <Body/>
-            <CardContainer clients={clients}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <CardContainer clients={clients}/>
+    </div>
+  );
+};
 
-export default App
+export default App;
