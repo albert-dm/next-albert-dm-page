8b7ac30ffc2e2ae56d30d8a0f413d49060bdbb3e
albert-dm <inbox@albert-dm.dev>
2020-09-17 18:16:36 -0300

    Reorganize folders

--- src/components/cardContainer/cardContainerStyled.styles.js
+++ src/components/cardContainer/cardContainerStyled.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { colors } from '../../theme';
+import { colors } from '../theme';
 
 export const CardContainerStyled = styled.div`
     @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import clients from './api/clientsList';
-import CardContainer from './components/cardContainer';
-import Body from './components/body';
-import Header from './components/header';
+import CardContainer from '../components/cardContainer';
+import Body from '../components/body';
+import Header from '../components/header';
 
 const App = () => {
   return (
