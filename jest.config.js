e0457b56bbd278214bbd3953d1b48446435609e4
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-10-08 18:40:33 -0300

    fix(coveralls): testing if the minimum coveralls is working

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
- 
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+
+  coverageThreshold: {
+    global: {
+
+      functions: 15,
+      lines: 15,
+      statements: 15,
+    },
+  },
 };
-
-
