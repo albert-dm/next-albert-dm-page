5d23cea62191412c7a4c769d9b0033a7daeaa392
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-10-08 18:11:42 -0300

    fix(coveralls): trying to fix a bug in coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,8 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  coverageThreshold: {
-    global: {
-
-      functions: 15,
-      lines: 15,
-      statements: 15,
-    },
-  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+ 
 };
 
 
