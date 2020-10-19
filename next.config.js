e6257cb92b7b1a1e9bdb10129d5d231c3fd89bf4
Barbara Neves <barbara.neves.oliveira@gmail.com>
2020-10-19 11:24:46 -0300

    fix(teste): teste

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const withMDX = require('@next/mdx')({
 });
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  distDir: 'build'
 });
 
 
